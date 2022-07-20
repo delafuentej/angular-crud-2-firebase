ba54c6fab783e49f94ba220fc39ddb1409321b91
Jesus de la Fuente <jfuentelucas@gmail.com>
2022-07-21 00:39:56 +0200

    alert message no items correction

--- src/app/models/employee.model.ts
+++ src/app/models/employee.model.ts
@@ -11,7 +11,7 @@ export class EmployeeModel{
         this.id="";
         this.firstName="";
         this.lastName="";
-        this.age=0;
+        this.age=0
         this.position="";
         this.email="";
         this.goodHealth=true;
